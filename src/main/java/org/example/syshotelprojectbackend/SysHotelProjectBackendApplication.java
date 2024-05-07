f916ab0eff320058c19e0602246a94c965b99951
Tobias <tobe.2309@hotmail.com>
2024-05-07 22:30:44 +0200

    Initial commit

---
+++ src/main/java/org/example/syshotelprojectbackend/SysHotelProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.syshotelprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysHotelProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysHotelProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/syshotelprojectbackend/SysHotelProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.syshotelprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysHotelProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
